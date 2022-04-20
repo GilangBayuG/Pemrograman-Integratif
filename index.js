7d4672dc291398f108b9fdb4cb721154ce4478db
GilangBayuG <gilangbayu48@gmail.com>
2022-04-20 11:08:00 +0700

    tes

---
+++ index.js
@@ -0,0 +1,48 @@
+var bodyParser = require('body-parser')
+var express = require('express')
+var app =  express();
+var jsonParser = bodyParser.json()
+// const port = 5500
+const port = process.env.PORT || 3000;
+
+const customers = [
+    {id:1, name:"Gilang"},
+    {id:2, name:"Bayu"},
+    {id:3, name:"Gumantara"}
+]   
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/customers', (req, res) => {
+    res.send(customers)
+
+  })
+
+app.get('/api/customers/:id', (req, res) => {
+    const customer = customers.find(c => c.id == parseInt(req.params.id));
+    if (!customer) {
+        res.status(404).send('Id not found !')
+    }
+    res.send(customer)
+}) 
+
+// POST method
+app.post('/api/customers', jsonParser, function (req,res) {
+    cur_id = customers.length + 1
+    
+    const customer = {
+        id: cur_id,
+        name: req.body.name
+    };
+
+    customers.push(customer);
+    res.send(customer);
+
+})
+
+// PORT CONF
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
